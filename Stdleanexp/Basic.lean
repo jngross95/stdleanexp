4f42a165a1dc1bd74cc06ad58132e9d413c05444
Jürgen Gross <jngross95@gmail.com>
2025-12-30 12:33:41 +0100

    first

--- Stdleanexp/Basic.lean
+++ Stdleanexp/Basic.lean
@@ -1,1 +1,8 @@
+
 def hello := "world"
+
+example (a b c : Nat) : a*(b+c) + a*b = a*b + a*c + a*b := by
+  rw[Nat.left_distrib]
+
+example (a b c d: Nat) : (a + (b + c)) + d = d + (c + a + b) := by
+  ac_rfl
